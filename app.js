75827eb540f4c67a765065e57c2aab3bd3ba7a18
Ganesh Surve <ganeshsurve296@gmail.com>
2026-04-01 00:46:00 +0530

    Added Styling to Main body and New form

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const Listing = require("./models/listing.js");
 const mongoose = require("mongoose");
-
+const ejsMate = require("ejs-mate");
+app.engine("ejs",ejsMate)
 const methodOverride = require("method-override");
 
 const path = require("path");
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
+
+app.use(express.static(path.join(__dirname,"/public")));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
--- models/listing.js
+++ models/listing.js
@@ -3,20 +3,24 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title:{
-        type : String,
-        requred :true
+    title: {
+        type: String,
+        required: true  
     },
-    description : String,
-    img:{
-        type:String,
-        default :"https://in.images.search.yahoo.com/search/images?p=unsplash+images&fr=mcafee&type=E210IN1589G0&imgurl=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1592093397571-36323190f372%3Ffm%3Djpg%26q%3D60%26w%3D3000%26ixlib%3Drb-4.0.3%26ixid%3DM3wxMjA3fDB8MHxzZWFyY2h8NHx8Y29tbWVyY2lhbCUyMHVzZXxlbnwwfHwwfHx8MA%253D%253D#id=-1&iurl=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1592093397571-36323190f372%3Ffm%3Djpg%26q%3D60%26w%3D3000%26ixlib%3Drb-4.0.3%26ixid%3DM3wxMjA3fDB8MHxzZWFyY2h8NHx8Y29tbWVyY2lhbCUyMHVzZXxlbnwwfHwwfHx8MA%253D%253D&action=click",
-        set : (v)=>v===""?"https://in.images.search.yahoo.com/search/images?p=unsplash+images&fr=mcafee&type=E210IN1589G0&imgurl=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1592093397571-36323190f372%3Ffm%3Djpg%26q%3D60%26w%3D3000%26ixlib%3Drb-4.0.3%26ixid%3DM3wxMjA3fDB8MHxzZWFyY2h8NHx8Y29tbWVyY2lhbCUyMHVzZXxlbnwwfHwwfHx8MA%253D%253D#id=-1&iurl=https%3A%2F%2Fimages.unsplash.com%2Fphoto-1592093397571-36323190f372%3Ffm%3Djpg%26q%3D60%26w%3D3000%26ixlib%3Drb-4.0.3%26ixid%3DM3wxMjA3fDB8MHxzZWFyY2h8NHx8Y29tbWVyY2lhbCUyMHVzZXxlbnwwfHwwfHx8MA%253D%253D&action=click":v
+    description: String,
+
+    image: {
+        filename: String,
+        url: {
+            type: String,
+            default: "https://images.unsplash.com/photo-1592093397571-36323190f372"
+        }
     },
-    price:Number,
-    location:String,
-    country:String
-})
+
+    price: Number,
+    location: String,
+    country: String
+});
 
 const Listing = mongoose.model("Listing",listingSchema);
 module.exports = Listing;
