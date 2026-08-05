0e42fa2b201f0d32a2b4cf81a80684585461411c
Ganesh Surve <ganeshsurve296@gmail.com>
2026-08-05 23:55:35 +0530

    Added form validation for user input to ensure correct inputs as per schema

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ app.get("/listings/:id",async (req,res)=>{
     let {id} = req.params;
     const listing = await Listing.findById(id);
     
-    res.render("listings/show",{listing});
+    res.render("listings/show.ejs",{listing});
 })
 
 
